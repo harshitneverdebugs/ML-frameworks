9d5616d6247c9d4c71586fce011491a580fde5a6
Harshit Singh <139043409+harshitneverdebugs@users.noreply.github.com>
2024-09-13 01:25:13 +0530

    ShowBatch function
    The show_batch function will display a grid of images from the first batch of the DataLoader dl. It removes the axis tick marks and shows the images in a clean grid format.

---
+++ show_batch.py
@@ -0,0 +1,8 @@
+from torchvision.utils import make_grid
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 6))
+        ax.set_xticks([]); ax.set_yticks([])
+        ax.imshow(make_grid(images, nrow=16).permute(1, 2, 0))
+        break
