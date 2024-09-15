ca0a07c268fa34569265f20642df189bb7777932
Harshit Singh <139043409+harshitneverdebugs@users.noreply.github.com>
2024-09-16 01:29:32 +0530

    Add files via upload

---
+++ pytorch-image-visualization.py
@@ -0,0 +1,12 @@
+def denormalize(images, means, stds):
+    means = torch.tensor(means).reshape(1, 3, 1, 1)
+    stds = torch.tensor(stds).reshape(1, 3, 1, 1)
+    return images * stds + means
+
+def show_batch(dl):
+    for images, labels in dl:
+        fig, ax = plt.subplots(figsize=(12, 12))
+        ax.set_xticks([]); ax.set_yticks([])
+        denorm_images = denormalize(images, *stats)
+        ax.imshow(make_grid(denorm_images[:64], nrow=8).permute(1, 2, 0).clamp(0,1))
+        break
